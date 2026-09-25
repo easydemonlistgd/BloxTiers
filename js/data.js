ca9308642355af1c6cea54e4e4e097b76249f32f
easydemonlistgd <redabebehe@gmail.com>
2026-09-25 23:53:14 +0300

    i am the best v2

--- js/data.js
+++ js/data.js
@@ -16,7 +16,7 @@ const kits = {
         name: "Sword",
 
         players: [
-            { name: "Reda_TNT", points: 50 },
+            { name: "Reda_TNT", points: 75 },
             { name: "User", points: 0 },
             { name: "User", points: 0 },
             { name: "User", points: 0 },
