22ea83084aac61817138a7cc3556ef4578add9f8
easydemonlistgd <redabebehe@gmail.com>
2026-09-25 23:55:53 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -17,7 +17,7 @@ const kits = {
 
         players: [
             { name: "Reda_TNT", points: 100 },
-            { name: "ITzMe", points: 50 },
+            { name: "ITzMe", points: 100 },
             { name: "User", points: 0 },
             { name: "User", points: 0 },
             { name: "User", points: 0 }
