f701c7ca6a80c523548caecc5f12b576c29f7422
easydemonlistgd <redabebehe@gmail.com>
2026-09-26 00:39:06 +0300

    worst update v0.424

--- js/data.js
+++ js/data.js
@@ -64,8 +64,8 @@ const kits = {
         name: "SMP",
 
         players: [
-            { name: "user", points: 0 },
-            { name: "user", points: 0 },
+            { name: "Reda_TNT", points: 0 },
+            { name: "ITzMeArzhel", points: 50 },
             { name: "user", points: 0 },
             { name: "user", points: 0 },
             { name: "user", points: 0 }
